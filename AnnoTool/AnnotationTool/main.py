8c1c0b16c750d7de8c9b8b7b75f450ec813eafd3
Achmet-h <91320230+Achmet-h@users.noreply.github.com>
2022-05-24 01:56:50 +0200

    loading image

--- AnnoTool/AnnotationTool/main.py
+++ AnnoTool/AnnotationTool/main.py
@@ -5,7 +5,10 @@
 #pyside6-uic filename.ui > filename.py
 
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui, QtCore
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import *
+
 from demo import Ui_MainWindow
 
 
@@ -13,9 +16,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        #connect buttons with functions
+        self.openButton.clicked.connect(self.displayMethod)
 
-    #here create your functions
+    #create your functions
+    def displayMethod(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open File", QtCore.QDir.currentPath())
+        if fileName:
+            image = QtGui.QImage(fileName)
+            if image.isNull():
+                QMessageBox.information(self, "Image Viewer",
+                                              "Cannot load %s." % fileName)
+                return
 
+            self.displayImage.setPixmap(QtGui.QPixmap.fromImage(image))
+            self.displayImage.resize(image.width(), image.height())
+            self.scaleFactor = 50
 
 
 app = QtWidgets.QApplication(sys.argv)
