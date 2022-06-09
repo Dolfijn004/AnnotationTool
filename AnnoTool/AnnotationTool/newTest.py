05f83a042d5ce149542d7907a0fdf0b4be6294ac
Dolfijn004 <jasonachtien18@gmail.com>
2022-06-09 14:41:43 +0200

    poging tot scrollbar en polygon

--- AnnoTool/AnnotationTool/newTest.py
+++ AnnoTool/AnnotationTool/newTest.py
@@ -15,6 +15,7 @@ from PIL import Image, ImageTk, ImageGrab
 WIDTH, HEIGHT = 1200, 800
 topx, topy, botx, boty = 0, 0, 0, 0
 rect_id = None
+polygon_point_coord = list()
 rect_list = list()
 rect_main_data = list()
 ImageFilePath = ""
@@ -24,10 +25,10 @@ ImageFound = False
 
 window = tk.Tk()
 window.title("Image Annotation Tool")
-window.geometry('%sx%s' % (WIDTH, HEIGHT))  # canvas is groter dan window op deze manier...maakt alles wonky als je t niet op zoomed zet 
+window.geometry('%sx%s' % (
+    WIDTH, HEIGHT))  # canvas is groter dan window op deze manier...maakt alles wonky als je het niet op zoomed zet
 window.state("zoomed")
 window.configure(background='grey')
-
 
 # creating style object to style the buttons
 style = Style()
@@ -59,10 +60,10 @@ def GetImageFilePath():
         ImgOpen = ImgOpen.resize((resized_width, resized_height), Image.ANTIALIAS)
         img = ImageTk.PhotoImage(ImgOpen)
         ImageFound = True
-        if (resized_width < image_area.winfo_width() - 4):
+        if resized_width < image_area.winfo_width() - 4:  # check if image is less wide than the canvas
             centering_width = (image_area.winfo_width() - resized_width) / 2
             image_area.create_image(centering_width, 0, image=img, anchor=NW)
-        elif (resized_height < image_area.winfo_height() - 4):
+        elif resized_height < image_area.winfo_height() - 4:  # check if image is taller than the canvas
             centering_height = (image_area.winfo_height() - resized_height) / 2
             image_area.create_image(0, centering_height, image=img, anchor=NW)
         else:
@@ -76,11 +77,11 @@ def GetImageFilePath():
 def resize_image(width, height):
     canvas_width = image_area.winfo_width() - 4
     canvas_height = image_area.winfo_height() - 4  # canvas width and height variables minus the border
-    if width/height > canvas_width/canvas_height:  # checking if dimensions are bigger then the canvas' dimensions
+    if width / height > canvas_width / canvas_height:  # checking if dimensions are bigger then the canvas' dimensions
         height = round(height * canvas_width / width)
         width = canvas_width
         return width, height
-    elif width/height < canvas_width/canvas_height:  # checking if dimensions are smaller than the canvas' dimensions
+    elif width / height < canvas_width / canvas_height:  # checking if dimensions are smaller than the canvas' dimensions
         width = round(width * canvas_height / height)
         height = canvas_height
         return width, height
@@ -98,13 +99,28 @@ def get_mouse_posn(event):
 def update_sel_rect(event):
     global botx, boty
     botx, boty = image_area.canvasx(event.x), image_area.canvasy(event.y)  # convert to real canvas coordinates
-    image_area.coords(rect_id, topx, topy, botx, boty)  # Update selection rect.
+    image_area.coords(rect_id, topx, topy, botx,
+                      boty)  # Update selection rect.   #  doet atm niks want rect_id wordt nergens aangemaakt
+    #  kan ook niet want rect wordt gemaakt op release event
 
 
 def draw_rect(self):
     draw_data = image_area.create_rectangle(topx, topy, botx, boty, outline="green", fill="")
     rect_list.append((topx, topy, botx, boty))
     rect_main_data.append(draw_data)
+
+
+def create_polygon():
+    if topx in polygon_point_coord and topy in polygon_point_coord:
+        image_area.create_polygon(polygon_point_coord)
+    else:
+        image_area.bind('<Button-1>', create_oval)
+        polygon_point_coord.append((topx, topy))
+
+
+def create_oval(event):
+    xcoord, ycoord = image_area.canvasx(event.x), image_area.canvasy(event.y)
+    image_area.create_oval((xcoord - 10, ycoord + 10, xcoord + 10, ycoord - 10))
 
 
 def cropImages():
@@ -134,7 +150,7 @@ def clearRectangles():
     global rect_main_data
     global rect_list
 
-    if (len(rect_main_data) > 0):
+    if len(rect_main_data) > 0:
         for rect in rect_main_data:
             image_area.delete(rect)
     rect_main_data.clear()
@@ -145,15 +161,15 @@ def clearRectangles():
 
 
 # Frame for buttons
-buttonFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20, relief= GROOVE)
+buttonFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20, relief=GROOVE)
 buttonFrame.grid(row=0, column=0, sticky='nsew')
 
 # frame for the canvas
-canvasFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20,  relief= GROOVE)
-canvasFrame.grid(row=0,  column=1, sticky='se')
+canvasFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20, relief=GROOVE)
+canvasFrame.grid(row=0, column=1, sticky='se')
 
 # left frame
-propertiesFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20, relief= GROOVE)
+propertiesFrame = Frame(window, height=window.winfo_height(), width=window.winfo_width(), borderwidth=20, relief=GROOVE)
 propertiesFrame.grid(row=0, column=2, sticky='ne')
 
 # grid configuration
@@ -161,7 +177,6 @@ window.rowconfigure(0, weight=3)
 window.columnconfigure(0, weight=1)
 window.columnconfigure(1, weight=3)
 window.columnconfigure(2, weight=1)
-
 
 # buttons
 openButton = Button(buttonFrame, text="Open", style="W.TButton", command=GetImageFilePath)
@@ -175,16 +190,17 @@ saveAsButton.grid(row=3, column=0)
 drawAnnotationBtn = Button(buttonFrame, text="Draw Rect", style="W.TButton", command=cropImages)
 drawAnnotationBtn.grid(row=4, column=0)
 createRecButton = Button(buttonFrame, text="Clear Annotations", style="W.TButton", command=clearRectangles)
-createRecButton.grid(row=5, column=0)
+createRecButton.grid(row=5, column=0)  # create button is clear button?
 zoomInButton = Button(buttonFrame, text="Zoom In", style="W.TButton")
 zoomInButton.grid(row=6, column=0)
 zoomOutButton = Button(buttonFrame, text="Zoom Out", style="W.TButton")
 zoomOutButton.grid(row=7, column=0)
-nextImage = Button(buttonFrame, text="Nxt Image", style="W.TButton")
+nextImage = Button(buttonFrame, text="Next Image", style="W.TButton")
 nextImage.grid(row=8, column=0)
-preImage = Button(buttonFrame, text="Pre Image", style="W.TButton")
+preImage = Button(buttonFrame, text="Prev Image", style="W.TButton")
 preImage.grid(row=9, column=0)
-
+createPolygonBtn = Button(buttonFrame, text="Create poly", style="W.TButton", command=create_polygon)
+createPolygonBtn.grid(row=10, column=0)
 
 # canvas
 image_area = Canvas(canvasFrame, width=1450, height=950)
@@ -210,6 +226,15 @@ folderList.grid(row=4, column=2)
 
 
 
+
+
+# scrollbar
+# scrollbarH = Scrollbar(canvasFrame, orient="horizontal", command=image_area.xview)
+# scrollbarV = Scrollbar(canvasFrame, orient="vertical", command=image_area.yview)
+# image_area.configure(yscrollcommand=scrollbarV.set, xscrollcommand=scrollbarH.set)
+# image_area.configure(scrollregion=(0, 0, 1000, 1000))
+# scrollbarH.grid(sticky="we")
+# scrollbarV.grid(sticky="ns")
 
 
 # menubar
@@ -242,17 +267,5 @@ helpmenu.add_command(label="Tutorial")
 helpmenu.add_command(label="About...")
 menubar.add_cascade(label="Help", menu=helpmenu)
 window.config(menu=menubar)
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
